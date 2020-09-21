c20142dd97c54b13ba4a3e3d1718f0d25d0927c3
Shayane Katchera <shaykatfsx@gmail.com>
2020-09-21 16:23:58 +0100

    First commit

---
+++ BigMapNav.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+"""
+PROGRAMME BIG MAP NAVIGATION
+C'est un projet pour simuler la navigation sur une grande map vu de haut, avec la camera qui regarde
+uniquement les allentours du joueur toujours au cente
+"""
+
+# TODO (21/09/2020): 
+#   - initialiser la fenetre
+#   - faire le design de la big map
+#   - afficher la BigMap en entier
+#   - reduire la distance d'affichage
+#   - implémenter le mouvement du joueur
+#   - implémenter les collisions 
+# ============== V1 Fini =====================
+#   - ajouter des couleurs
+#   - ajouter l'implementation d'étages
+#   - implémentation de lumiere
+#   - implémentation de dynamique light
+#   - implémentation des ombres
+# ============== V2 Fini =====================
+#   - exporter de tkinter vers pygame
+#   - optimiser le code
+#   - ajouter des IAs (dans un premier temps dans un fichier test pour inclure du pathfinding)
+# ============== V3 Fini =====================
